6fa7c0c3b9e25d6550424b6a64943d7951074b44
vantichelen <vantichelen@192.168.1.108>
2018-12-27 15:37:32 +0100

    klasse toegevoegd, datamembers, constructor, getters en setters

---
+++ src/breukoefening/BreukOefening.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package breukoefening;
+
+/**
+ *
+ * @author vantichelen
+ */
+public class BreukOefening {
+    private int teller;
+    private int noemer;
+
+
+    public BreukOefening(int teller, int noemer) {
+        this.teller = teller;
+        this.noemer = noemer;
+    }
+
+    /**
+     * @return the teller
+     */
+    public int getTeller() {
+        return teller;
+    }
+
+    /**
+     * @param teller the teller to set
+     */
+    public void setTeller(int teller) {
+        this.teller = teller;
+    }
+
+    /**
+     * @return the noemer
+     */
+    public int getNoemer() {
+        return noemer;
+    }
+
+    /**
+     * @param noemer the noemer to set
+     */
+    public void setNoemer(int noemer) {
+        this.noemer = noemer;
+    }
+    
+    
+    
+    
+}
